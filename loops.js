c947f4ac44f0baeeb595f761fb1d15fe01a01555
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-08-13 17:35:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ const myArray = []
 
 function forLoop(myArray){
   for (var i = 0; i < 25; i++){
-    myArray.push('I am ${i} strange loops.')
+    myArray.push('I am i strange loop.')
   }
   return myArray
 }
