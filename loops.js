2009574f468d88ac1c6c5894f61d95787f0647c6
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-08-13 17:44:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,12 @@ const myArray = []
 
 function forLoop(myArray){
   for (let i = 0; i < 25; i++){
+    //myArray.push('I am ${i} strange loops.')
+  if(i == 1){
+    myArray.push('I am 1 strange loop.')}
+  else
     myArray.push('I am ${i} strange loops.')
   }
+  
   return myArray
 }
